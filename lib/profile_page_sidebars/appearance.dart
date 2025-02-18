e96f8d1cad1d19ca1355bcd89a3fba137d1b009b
SHOUVIK98 <shouvik.misra001@gmail.com>
2025-02-18 16:35:58 +0530

    appearance page change ui

--- lib/profile_page_sidebars/appearance.dart
+++ lib/profile_page_sidebars/appearance.dart
@@ -91,8 +91,109 @@
 //   }
 // }
 
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import '../theme/color_picker_selection.dart';
+// import '../theme/theme_selector.dart';
+//
+// class Appearance extends StatefulWidget {
+//   final ColorScheme colorScheme;
+//   final ThemeMode themeMode;
+//   final Function(bool isDarkMode) onThemeChanged;
+//   final Function(ColorScheme colorScheme) onColorSchemeChanged;
+//
+//   const Appearance({
+//     Key? key,
+//     required this.onThemeChanged,
+//     required this.onColorSchemeChanged,
+//     required this.colorScheme,
+//     required this.themeMode,
+//   }) : super(key: key);
+//
+//   @override
+//   _AppearanceState createState() => _AppearanceState();
+// }
+//
+// class _AppearanceState extends State<Appearance> {
+//   bool _isDarkMode = false;
+//   late ColorScheme _colorScheme;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _isDarkMode = widget.themeMode == ThemeMode.dark;
+//     _colorScheme = widget.colorScheme;
+//   }
+//
+//   void _toggleTheme(bool isDark) {
+//     setState(() {
+//       _isDarkMode = isDark;
+//       _colorScheme = _isDarkMode
+//           ? ColorScheme.fromSwatch(brightness: Brightness.dark)
+//           : ColorScheme.fromSwatch(brightness: Brightness.light);
+//     });
+//     widget.onThemeChanged(_isDarkMode);
+//     widget.onColorSchemeChanged(_colorScheme);
+//   }
+//
+//   void _updateColorScheme(ColorScheme newScheme) {
+//     setState(() {
+//       _colorScheme = newScheme;
+//     });
+//     widget.onColorSchemeChanged(_colorScheme);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoApp(
+//       theme: CupertinoThemeData(
+//         primaryColor: _colorScheme.primary,
+//         brightness: _isDarkMode ? Brightness.dark : Brightness.light,
+//       ),
+//       home: CupertinoPageScaffold(
+//         navigationBar: CupertinoNavigationBar(
+//           middle: Text(
+//             'Appearance Settings',
+//               style: TextStyle(color: widget.colorScheme.primary,fontSize: 22),
+//           ),
+//           leading: CupertinoButton(
+//             padding: EdgeInsets.zero,
+//             child:  Icon(
+//               CupertinoIcons.back,
+//               color: _isDarkMode ? CupertinoColors.white : CupertinoColors.black, // Dark mode icon color
+//               size: 32, // Set the size to make it larger
+//             ),
+//             onPressed: () => Navigator.pop(context),
+//           ),
+//         ),
+//         child: SafeArea(
+//           child: SingleChildScrollView(
+//             child: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               children: [
+//                 ThemeSelector(
+//                   isDarkMode: _isDarkMode,
+//                   onThemeSelected: _toggleTheme,
+//                   colorScheme: widget.colorScheme,
+//                 ),
+//                 const SizedBox(height: 20),
+//                 // ColorPickerSection(
+//                 //   colorScheme: _colorScheme,
+//                 //   onColorSchemeChanged: _updateColorScheme,
+//                 // ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+////////////// ui change new ///////////////////////
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../theme/color_picker_selection.dart';
 import '../theme/theme_selector.dart';
 
@@ -145,47 +246,101 @@ class _AppearanceState extends State<Appearance> {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      theme: CupertinoThemeData(
-        primaryColor: _colorScheme.primary,
-        brightness: _isDarkMode ? Brightness.dark : Brightness.light,
-      ),
-      home: CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text(
-            'Appearance Settings',
-              style: TextStyle(color: widget.colorScheme.primary,fontSize: 22),
-          ),
-          leading: CupertinoButton(
-            padding: EdgeInsets.zero,
-            child:  Icon(
-              CupertinoIcons.back,
-              color: _isDarkMode ? CupertinoColors.white : CupertinoColors.black, // Dark mode icon color
-              size: 32, // Set the size to make it larger
-            ),
-            onPressed: () => Navigator.pop(context),
+    return CupertinoPageScaffold(
+      backgroundColor: _isDarkMode ? CupertinoColors.black : CupertinoColors.white,
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: _isDarkMode ? CupertinoColors.darkBackgroundGray : CupertinoColors.white,
+        middle: Text(
+          'Appearance Settings',
+          style: TextStyle(
+            color: _isDarkMode ? CupertinoColors.white : CupertinoColors.black,
+            decoration: TextDecoration.none,
+            decorationColor: CupertinoColors.transparent,
           ),
         ),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ThemeSelector(
-                  isDarkMode: _isDarkMode,
-                  onThemeSelected: _toggleTheme,
-                  colorScheme: widget.colorScheme,
+        leading: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Icon(CupertinoIcons.back, color: _isDarkMode ? CupertinoColors.white : CupertinoColors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Icon(CupertinoIcons.bell, color: _isDarkMode ? CupertinoColors.white : CupertinoColors.black),
+          onPressed: () {},
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: CupertinoColors.activeGreen,
                 ),
-                const SizedBox(height: 20),
-                // ColorPickerSection(
-                //   colorScheme: _colorScheme,
-                //   onColorSchemeChanged: _updateColorScheme,
-                // ),
-              ],
-            ),
+                child: SvgPicture.asset(
+                  'assets/appearancePageBg.svg',
+                  width: 220,
+                  height: 220,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'CHOOSE A STYLE',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: _isDarkMode ? CupertinoColors.white : CupertinoColors.black,
+                  decoration: TextDecoration.none,
+                  decorationColor: CupertinoColors.transparent,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Would you like to change appearance?\nCustomize your interface',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: _isDarkMode ? CupertinoColors.lightBackgroundGray : CupertinoColors.darkBackgroundGray,
+                  decoration: TextDecoration.none,
+                  decorationColor: CupertinoColors.transparent,
+                ),
+              ),
+              const SizedBox(height: 20),
+              CupertinoSlidingSegmentedControl<bool>(
+                groupValue: _isDarkMode,
+                backgroundColor: _isDarkMode ? CupertinoColors.darkBackgroundGray : CupertinoColors.lightBackgroundGray,
+                thumbColor: _isDarkMode ? CupertinoColors.white : CupertinoColors.black,
+                children: {
+                  false: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(CupertinoIcons.sun_max_fill, color: _isDarkMode ? CupertinoColors.white : CupertinoColors.white),
+                  ),
+                  true: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(CupertinoIcons.moon_fill, color: _isDarkMode ? CupertinoColors.black : CupertinoColors.white),
+                  ),
+                },
+                onValueChanged: (value) {
+                  if (value != null) {
+                    _toggleTheme(value);
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
